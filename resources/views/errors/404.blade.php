403aa6146978a9320bbb64a1db2ab90f1b483631
ledhcg <dinhcuong.firewin99@gmail.com>
2022-03-04 13:39:07 +0300

    Update 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,8 +8,8 @@
     <div class="container py-5 mb-lg-5">
         <div class="row justify-content-center pt-lg-4 text-center">
             <div class="col-lg-5 col-md-7 col-sm-9"><img class="d-block mx-auto mb-5" src="{{asset('public/main/img/pages/404.png')}}" width="340" alt="404 Error">
-                <h1 class="h3 text-light">{{__('main.404 error')}}</h1>
-                <h3 class="h5 fw-normal mb-4 text-light">{{__('main.We cant seem to find the page you are looking for')}}</h3>
+                <h1 class="h3">{{__('main.404 error')}}</h1>
+                <h3 class="h5 fw-normal mb-4">{{__('main.We cant seem to find the page you are looking for')}}</h3>
                 <a class="btn btn-primary fs-md mb-5">
                     {{__('main.Home')}}
                 </a>
